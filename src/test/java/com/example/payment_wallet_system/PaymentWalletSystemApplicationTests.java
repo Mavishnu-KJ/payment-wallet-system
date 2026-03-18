b50878d0bd44f9db730fb9630b32d67452cde0c9
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-03-18 12:18:29 +0530

    Initial setup

---
+++ src/test/java/com/example/payment_wallet_system/PaymentWalletSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payment_wallet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWalletSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
