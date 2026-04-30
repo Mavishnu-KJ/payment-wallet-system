e5d9c0731451626fa105e83299202552af5e8efe
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-04-30 19:09:19 +0530

    Clean GlobalExceptionHandler

--- transaction-service/src/main/java/com/example/transactionservice/config/GlobalExceptionHandler.java
+++ transaction-service/src/main/java/com/example/transactionservice/config/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.example.transactionservice.config;
 
+import com.example.transactionservice.exceptions.InsufficientBalanceException;
 import com.example.transactionservice.exceptions.ResourceNotFoundException;
 import com.example.transactionservice.model.dto.ErrorResponse;
 import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
@@ -34,6 +40,90 @@ public class GlobalExceptionHandler {
         int statusCode = e.status();
 
         return new ResponseEntity<>(error, HttpStatus.valueOf(statusCode));
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    ResponseEntity<ErrorResponse> handleInsufficientBalanceException(InsufficientBalanceException e) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Insufficient Balance",
+                List.of(e.getMessage()),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    //Validation Errors - @Valid @RequestBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        List<String> errorList = e.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() +" : "+err.getDefaultMessage())
+                .toList();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Failed",
+                errorList,
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    //For @Valid on PathVariable / RequestParam
+    //eg. @Positive(message = "minSalary must be greater than 0") @RequestParam (name="minSalary", required = false) int salary falls in global error
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    ResponseEntity<ErrorResponse> handleHandlerMethodValidationException(HandlerMethodValidationException e){
+
+        List<String> errorList = new ArrayList<>();
+
+        e.getAllErrors().forEach(err->{
+            if(err instanceof FieldError fieldError){
+                errorList.add(fieldError.getField() + " : " +fieldError.getDefaultMessage());
+            }else{
+                //Rare global error
+                errorList.add("global : "+err.getDefaultMessage());
+            }
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Handler Method Validation Failed",
+                errorList,
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    //eg. GetMapping("/api/employees/{id}", /api/employees/abc)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+
+        List<String> errorList = List.of("Invalid parameter : "+e.getName()+" Expected type "+e.getRequiredType().getSimpleName() +" but, got "+e.getValue());
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Type mismatch",
+                errorList,
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+    }
+
+    //For other unhandled exceptions
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<ErrorResponse> handleGenericErrors(Exception e){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "INTERNAL SERVER ERROR",
+                List.of("Unexpected error : "+e.getMessage()),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
--- user-service/src/main/java/com/example/userservice/config/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/example/userservice/config/GlobalExceptionHandler.java
@@ -4,15 +4,32 @@ import com.example.userservice.exceptions.ResourceNotFoundException;
 import com.example.userservice.model.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e){
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Resource Not Found",
+                List.of(e.getMessage()),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 
     //NOTE : MethodArgumentNotValidException (for @Valid @RequestBody)
     //Eg. /addEmployee, name must not be blank, salary must be greater than 0
@@ -33,17 +50,46 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e){
+    //For @Valid on PathVariable / RequestParam
+    //eg. @Positive(message = "minSalary must be greater than 0") @RequestParam (name="minSalary", required = false) int salary falls in global error
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    ResponseEntity<ErrorResponse> handleHandlerMethodValidationException(HandlerMethodValidationException e){
+
+        List<String> errorList = new ArrayList<>();
+
+        e.getAllErrors().forEach(err->{
+            if(err instanceof FieldError fieldError){
+                errorList.add(fieldError.getField() + " : " +fieldError.getDefaultMessage());
+            }else{
+                //Rare global error
+                errorList.add("global : "+err.getDefaultMessage());
+            }
+        });
 
         ErrorResponse errorResponse = new ErrorResponse(
-                HttpStatus.NOT_FOUND.value(),
-                "Resource Not Found",
-                List.of(e.getMessage()),
+                HttpStatus.BAD_REQUEST.value(),
+                "Handler Method Validation Failed",
+                errorList,
                 LocalDateTime.now()
         );
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    //eg. GetMapping("/api/employees/{id}", /api/employees/abc)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+
+        List<String> errorList = List.of("Invalid parameter : "+e.getName()+" Expected type "+e.getRequiredType().getSimpleName() +" but, got "+e.getValue());
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Type mismatch",
+                errorList,
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
     }
 
     @ExceptionHandler(Exception.class)
