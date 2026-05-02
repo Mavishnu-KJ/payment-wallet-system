09d2da107c879fbd96d24d327a572a0c53efac1f
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-05-02 18:33:13 +0530

    notification-service initial setup

---
+++ notification-service/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.example.notificationservice.client")
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
