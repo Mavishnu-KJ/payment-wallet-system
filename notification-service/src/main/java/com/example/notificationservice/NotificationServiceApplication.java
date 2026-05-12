5eab43803b63e18f165f915c94560db69c74d3ac
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-05-12 15:34:43 +0530

    Eureka module

--- notification-service/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(exclude = {
         UserDetailsServiceAutoConfiguration.class
 })
-@EnableFeignClients(basePackages = "com.example.notificationservice.client")
+//@EnableFeignClients(basePackages = "com.example.notificationservice.client")
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
