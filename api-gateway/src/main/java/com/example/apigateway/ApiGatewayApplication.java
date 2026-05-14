c8ad6c6b633f12c0f63171c018cd9352ba1825cc
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-05-14 21:07:18 +0530

    Resilence4J - RateLimiter filter

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,22 @@
 package com.example.apigateway;
 
 
+import com.example.apigateway.filter.RateLimitingFilter;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RateLimitingFilter rateLimitingFilter(RateLimiterRegistry rateLimiterRegistry) {
+        return new RateLimitingFilter(rateLimiterRegistry);
     }
 }
